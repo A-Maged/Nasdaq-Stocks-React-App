240a397c49b7e4880efa419b2ae6a7e3e08fd37d
a-maged <maged.dev@gmail.com>
2021-10-21 11:53:00 +0200

    feat: add react-query

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,15 +1,24 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { HelmetProvider } from 'react-helmet-async';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import { Routes } from '../routes';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { refetchOnWindowFocus: false },
+  },
+});
 
 export function App() {
   return (
     <HelmetProvider>
       <ChakraProvider>
         <Router>
-          <Routes />
+          <QueryClientProvider client={queryClient}>
+            <Routes />
+          </QueryClientProvider>
         </Router>
       </ChakraProvider>
     </HelmetProvider>
