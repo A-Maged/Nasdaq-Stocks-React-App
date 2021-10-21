221a2f2f1d05e41a535f82d45e35b245989f4e0f
a-maged <maged.dev@gmail.com>
2021-10-22 00:14:54 +0200

    style: make logo correctly centered in home page

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -14,23 +14,23 @@ export function Home() {
         maxW: 'container.md',
       }}
     >
-      <Center h="90vh">
-        <Link to="/explore">
-          <Image
-            w="64"
-            src={process.env.PUBLIC_URL + 'logo-white.png'}
-            alt="logo"
-          />
+      <Center h="100vh" flexDirection="column">
+        <Center flexGrow={1} mt={15}>
+          <Link to="/explore">
+            <Image
+              w="64"
+              src={process.env.PUBLIC_URL + 'logo-white.png'}
+              alt="logo"
+            />
 
-          <Text fontSize="md" textAlign="center" mt="4">
-            Click to explore stocks
-            <ArrowForwardIcon ml="2" />
-          </Text>
-        </Link>
-      </Center>
+            <Text fontSize="md" textAlign="center" mt="4">
+              Click to explore stocks
+              <ArrowForwardIcon ml="2" />
+            </Text>
+          </Link>
+        </Center>
 
-      <Center>
-        <Text fontSize="sm" textAlign="center" mt="2">
+        <Text fontSize="sm" textAlign="center" pb="3">
           By <br /> Abdelrahman Maged
         </Text>
       </Center>
