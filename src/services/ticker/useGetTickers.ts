cb9d5bbdceaaaf261d6088397607eeb6950160a1
a-maged <maged.dev@gmail.com>
2021-10-22 08:17:07 +0200

    feat: show error when query fails

--- src/services/ticker/useGetTickers.ts
+++ src/services/ticker/useGetTickers.ts
@@ -1,6 +1,7 @@
 import { useInfiniteQuery } from 'react-query';
 import { AxiosError } from 'axios';
 import { QueryFunction, QueryKey } from 'react-query';
+import { useToast } from '@chakra-ui/react';
 
 import {
   TickerApiError,
@@ -9,12 +10,22 @@ import {
 } from 'api/repos/TickerRepo';
 
 export function useGetTickers(limit = 10) {
+  const toast = useToast();
+
   return useInfiniteQuery<TickerListReturn, AxiosError<TickerApiError>>(
     'tickers',
     makeFetchTickers(limit),
     {
       getNextPageParam: (apiResponse) => apiResponse?.next_url,
       refetchOnMount: false,
+      onError: (error) => {
+        toast({
+          title: error?.response?.data.error,
+          status: 'warning',
+          isClosable: true,
+          position: 'bottom-right',
+        });
+      },
     }
   );
 }
